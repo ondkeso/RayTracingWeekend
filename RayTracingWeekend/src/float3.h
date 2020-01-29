2bd84821cf3f721b4762989326ea48834b13ee1e
Tobias Luckey <tobias.luckey@paradoxinteractive.com>
2020-01-29 13:03:49 +0100

    Add hittable virtual class

--- RayTracingWeekend/src/float3.h
+++ RayTracingWeekend/src/float3.h
@@ -4,7 +4,8 @@
 
 #pragma warning(push)
 #pragma warning(disable : 26495)
-class float3 {
+class float3
+{
 public:
 
 	float3() {}
--- RayTracingWeekend/src/hittable.h
+++ RayTracingWeekend/src/hittable.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "ray.h"
+
+struct hitRecord
+{
+	float t;
+	float3 position;
+	float3 normal;
+};
+
+class hittable
+{
+public:
+	virtual bool hit(const ray& r, float t_min, float t_max, hitRecord& record) const = 0;
+};
