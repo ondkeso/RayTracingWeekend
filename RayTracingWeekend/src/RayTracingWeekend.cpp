3326f1400e78e1e8c866553514a0d756cb0dac86
Tobias Luckey <tobias.luckey@paradoxinteractive.com>
2020-01-29 12:50:32 +0100

    Add sphere hit function and test for it
    Also fix calculating the v coordinate incorrectly

--- RayTracingWeekend/src/RayTracingWeekend.cpp
+++ RayTracingWeekend/src/RayTracingWeekend.cpp
@@ -6,21 +6,49 @@ constexpr float PI2 = 2.0f * PI;
 #include "ray.h"
 #include <fstream>
 
+float hit_sphere(const float3& center, float radius, const ray& r)
+{
+	float3 center_to_origin = r.origin - center;
+	float a = dot(r.direction, r.direction);
+	float b = 2.0f * dot(center_to_origin, r.direction);
+	float c = dot(center_to_origin, center_to_origin) - radius * radius;
+	float discriminant = b * b - 4.0f * a * c;
+
+	if (discriminant < 0.0f)
+	{
+		return -1.0f;
+	} 
+	else 
+	{
+		return (-b - sqrt(discriminant)) / (2.0f * a);
+	}
+}
+
 float3 colorOfRay(const ray& r)
 {
+	//Test sphere
+	{
+		const float3 center{ 0.0f, 0.0f, 1.001f };
+		float hit = hit_sphere(center, 0.5f, r);
+		if (hit > 0.0f)
+		{
+			float3 normal = unit_vector(r.position_at(hit) - center);
+			return 0.5f * float3{ normal.x + 1.0f, normal.y + 1.0f, normal.z + 1.0f };
+		}
+	}
+
 	float3 unit_direction = unit_vector(r.direction);
 	float t = 0.5f * (unit_direction.y + 1.0f);
-	return lerp(float3(1.0f, 1.0f, 1.0f), float3(0.5f, 0.7f, 1.0f), t);
+	return lerp(float3{ 1.0f, 1.0f, 1.0f }, float3{ 0.5f, 0.7f, 1.0f }, t);
 }
 
 int main()
 {
-	constexpr int nx = 500;
-	constexpr int ny = 300;
-	constexpr float aspect_ratio = nx / (float)ny;
-	constexpr float3 lower_left_corner(-aspect_ratio * 0.5f, -1.0f / aspect_ratio * 0.5f, 1.0f);
-	constexpr float3 horizontal(aspect_ratio, 0.0f, 0.0f);
-	constexpr float3 vertical(0.0f, 1 / aspect_ratio, 0.0f);
+	constexpr int nx = 400;
+	constexpr int ny = 200;
+	constexpr float3 lower_left_corner(-2.0f, -1.0f, 1.0f);
+	constexpr float3 horizontal(4.0f, 0.0f, 0.0f);
+	constexpr float3 vertical(0.0f, 2.0f, 0.0f);
 	constexpr float3 origin(0.0f, 0.0f, 0.0f);
 
 	std::ofstream cout("output.ppm");
@@ -31,7 +59,7 @@ int main()
 		for (int i = 0; i < nx; i++)
 		{
 			float u = (float)i / (float)nx;
-			float v = (float)j / (float)nx;
+			float v = (float)j / (float)ny;
 
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
 
