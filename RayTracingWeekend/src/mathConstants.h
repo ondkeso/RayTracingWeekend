0940398b2e000fc24d605ae4aab9ac677a871b05
Tobias Luckey <tobias.luckey@paradoxinteractive.com>
2020-05-10 20:09:35 +0200

    constexpr and pragma once the math defines

--- RayTracingWeekend/src/mathConstants.h
+++ RayTracingWeekend/src/mathConstants.h
@@ -1,11 +1,15 @@
-#define E        2.7182818284590452353f  // e
-#define LOG2E    1.4426950408889634073f  // log2(e)
-#define LOG10E   0.4342944819032518276f  // log10(e)
-#define LN2      0.6931471805599453094f  // ln(2)
-#define LN10     2.3025850929940456840f  // ln(10)
-#define PI       3.1415926535897932384f  // pi
-#define PI_2     1.5707963267948966192f  // pi/2
-#define PI_4     0.7853981633974483096f  // pi/4
-#define PI2      6.2831853071795864769f  // pi*2
-#define SQRT2    1.4142135623730950488f  // sqrt(2)
-#define SQRT1_2  0.7071067811865475244f  // 1/sqrt(2)
+#pragma once
+
+static constexpr float E = 2.7182818284590452353f;  // e
+static constexpr float LOG2E = 1.4426950408889634073f;  // log2(e)
+static constexpr float LOG10E = 0.4342944819032518276f;  // log10(e)
+static constexpr float LN2 = 0.6931471805599453094f;  // ln(2)
+static constexpr float LN10 = 2.3025850929940456840f;  // ln(10)
+static constexpr float PI = 3.1415926535897932384f;  // pi
+static constexpr float PI_2 = 1.5707963267948966192f;  // pi/2
+static constexpr float PI_4 = 0.7853981633974483096f;  // pi/4
+static constexpr float PI2 = 6.2831853071795864769f;  // pi*2
+static constexpr float SQRT2 = 1.4142135623730950488f;  // sqrt(2)
+static constexpr float INV_SQRT2 = 0.7071067811865475244f;  // 1/sqrt(2)
+static constexpr float RAD_TO_DEG = 180.0f / PI;
+static constexpr float DEG_TO_RAD = PI / 180.0f;
